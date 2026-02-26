16a04123726a990496cf19fa474dd90028a152c0
DistantCandle <loftuswp2@gmail.com>
2026-02-26 03:34:01 -0500

    hopefully fixed register functionality

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -51,34 +51,40 @@ def logout_request(request):
 def registration(request):
     context = {}
 
-    # Load JSON data from the request body
+    # Load JSON data from request body
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
     first_name = data['firstName']
     last_name = data['lastName']
     email = data['email']
+
     username_exist = False
-    email_exist = False
-    
+
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
     except:
-        # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
+        logger.debug(f"{username} is a new user")
 
-    # If it is a new user
+    # If new user → create account
     if not username_exist:
-        # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
-        # Login the user and redirect to list page
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email
+        )
+
+        # Log in the user
         login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
+
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
-        data = {"userName":username,"error":"Already Registered"}
+    else:
+        data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 def get_cars(request):
